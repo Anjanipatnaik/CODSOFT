4e9379be29c5b2028e8cd1502be05bb3f34ccbcf
Anjanipatnaik <142499952+Anjanipatnaik@users.noreply.github.com>
2024-04-07 13:00:51 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,53 @@
+def calculate():
+    
+    number_1 = int(input('Enter your first number: '))
+    number_2 = int(input('Enter your second number: '))
+
+    operation = input('''
+math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        try:
+            print(number_1 / number_2)
+        except ZeroDivisionError:
+            print('Error: Division by zero is not allowed.')
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+
+    else:
+        again()
+
+calculate()
+
